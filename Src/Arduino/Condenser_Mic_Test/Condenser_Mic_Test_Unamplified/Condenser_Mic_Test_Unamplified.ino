aad82b7abfa634e6cbd0b6e06930e2dcacdf13fd
Alex Trujillo <atrujil2@cord.edu>
2023-11-20 13:48:07 -0600

    Created test for unamplified arduino mic

---
+++ Src/Arduino/Condenser_Mic_Test/Condenser_Mic_Test_Unamplified/Condenser_Mic_Test_Unamplified.ino
@@ -0,0 +1,32 @@
+const int microphonePin = A0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int mn = 1024;
+  int mx = 0;
+  
+  for (int i = 0; i < 1000; i++) {
+
+    int val = analogRead(microphonePin);
+
+    mn = min(mn, val);
+    mx = max(mx, val);
+
+  }
+
+  int delta = mx - mn;
+  Serial.print("\nMin = ");
+  Serial.print(mn);
+  Serial.print("\nMax = ");
+  Serial.print(mx);
+  Serial.print("\nDelta = ");
+  Serial.print(delta);
+
+}
